4dc782d5202a0eda7049411e8e9653e8a16cec05
AlessandroB1993 <ale.berg.93@gmail.com>
2024-11-28 17:52:44 +0100

    Added the readme

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,10 +126,6 @@ const controlAddRecipe = async function (newRecipe) {
   }, MODAL_CLOSE_SEC * 1300);
 }
 
-const newFeature = () => {
-  console.log('Welcome to the application!');
-}
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -138,7 +134,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
 
