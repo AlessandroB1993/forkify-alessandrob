54eb11f112d11a52bd919d61846cd1c618b47b03
AlessandroB1993 <ale.berg.93@gmail.com>
2025-04-28 20:11:19 +0200

    fixed svg icon

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -1,51 +1,55 @@
-import { RES_PER_PAGE } from '../config.js';
-import View from './View.js';
-import icons from 'url:../../img/icons.svg';
-
+import { RES_PER_PAGE } from "../config.js";
+import View from "./View.js";
+import icons from "url:../../img/icons.svg";
 
 class PaginationView extends View {
-    _parentEl = document.querySelector('.pagination');
-    _pagesNum = document.querySelector('.pagination__num')
+  _parentEl = document.querySelector(".pagination");
+  _pagesNum = document.querySelector(".pagination__num");
 
-    addHandlerClick(handler) {
-        this._parentEl.addEventListener('click', function (e) {
-            const btn = e.target.closest('.btn--inline');
+  addHandlerClick(handler) {
+    this._parentEl.addEventListener("click", function (e) {
+      const btn = e.target.closest(".btn--inline");
 
-            if (!btn) return;
-            const goToPage = +btn.dataset.goto;
-            handler(goToPage);
-        })
-    }
+      if (!btn) return;
+      const goToPage = +btn.dataset.goto;
+      handler(goToPage);
+    });
+  }
 
-    _generateMarkup() {
-        const currentPage = this._data.page;
-        const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
+  _generateMarkup() {
+    const currentPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
 
-        return this._generateMarkupBtn(currentPage, numPages);
-    }
+    return this._generateMarkupBtn(currentPage, numPages);
+  }
 
-    _generateMarkupBtn(curPage, numPages) {
-        // Page 1, and there are other pages
-        if (curPage === 1 && numPages > 1) {
-            return `
+  _generateMarkupBtn(curPage, numPages) {
+    // Page 1, and there are other pages
+    if (curPage === 1 && numPages > 1) {
+      return `
             <div class="pagination__num">
             <span>${curPage} of ${numPages}</span>
           </div>
 
-            <button data-goto="${curPage + 1}" class="btn--inline pagination__btn--next">
+            <button data-goto="${
+              curPage + 1
+            }" class="btn--inline pagination__btn--next">
             <span>Page ${curPage + 1}</span>
             <svg class="search__icon">
-              <use href="${icons}.svg#icon-arrow-right"></use>
+              <use href="${icons}#icon-arrow-right"></use>
             </svg>
             </button>
           `;
-        }
+    }
 
-
-        // Last page
-        if (curPage === numPages && numPages > 1) {
-            return `
-            <button data-goto="${curPage - 1}" class="btn--inline pagination__btn--prev">
+    // Last page
+    if (curPage === numPages && numPages > 1) {
+      return `
+            <button data-goto="${
+              curPage - 1
+            }" class="btn--inline pagination__btn--prev">
             <svg class="search__icon">
               <use href="${icons}#icon-arrow-left"></use>
             </svg>
@@ -56,12 +60,14 @@ class PaginationView extends View {
             <span>${curPage} of ${numPages}</span>
           </div>
             `;
-        }
+    }
 
-        // Other page
-        if (curPage < numPages) {
-            return `
-            <button data-goto="${curPage - 1}" class="btn--inline pagination__btn--prev">
+    // Other page
+    if (curPage < numPages) {
+      return `
+            <button data-goto="${
+              curPage - 1
+            }" class="btn--inline pagination__btn--prev">
             <svg class="search__icon">
               <use href="${icons}#icon-arrow-left"></use>
             </svg>
@@ -72,18 +78,20 @@ class PaginationView extends View {
              <span>${curPage} of ${numPages}</span>
           </div>
 
-           <button data-goto="${curPage + 1}" class="btn--inline pagination__btn--next">
+           <button data-goto="${
+             curPage + 1
+           }" class="btn--inline pagination__btn--next">
             <span>Page ${curPage + 1}</span>
             <svg class="search__icon">
               <use href="${icons}.svg#icon-arrow-right"></use>
             </svg>
             </button>
         `;
-        }
-
-        // Page 1, and there are NO other pages
-        return ''
     }
+
+    // Page 1, and there are NO other pages
+    return "";
+  }
 }
 
 export default new PaginationView();
