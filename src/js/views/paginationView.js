c104688429c2c2afc5566c7b1e9cbe85ba7f5cdf
AlessandroB1993 <ale.berg.93@gmail.com>
2025-04-28 20:20:54 +0200

    fixed svg icon

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -83,7 +83,7 @@ class PaginationView extends View {
            }" class="btn--inline pagination__btn--next">
             <span>Page ${curPage + 1}</span>
             <svg class="search__icon">
-              <use href="${icons}.svg#icon-arrow-right"></use>
+              <use href="${icons}#icon-arrow-right"></use>
             </svg>
             </button>
         `;
